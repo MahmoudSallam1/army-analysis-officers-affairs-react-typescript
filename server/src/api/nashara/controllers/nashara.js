bf9e40f1787c234e4d46fa993456a694320b3ea3
MamhoudSallam1 <sallam@msallam.me>
2023-01-11 13:10:15 +0200

    remvoed server folder

--- server/src/api/nashara/controllers/nashara.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * nashara controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::nashara.nashara');
